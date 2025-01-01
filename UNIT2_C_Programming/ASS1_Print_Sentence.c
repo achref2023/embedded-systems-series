68d583cea37cd0451ef978f1cc5f1e905484a3bd
achref <achrfe.gmail.com>
2025-01-01 23:23:35 +0000

    dossierunit2

---
+++ UNIT2_C_Programming/ASS1_Print_Sentence.c
@@ -0,0 +1,17 @@
+/*
+ ======================================================================================================================
+ Name        : EX1_Print_Sentence.c
+ Author      : Achref uthmani
+ Created on	 : jan 01, 2025
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX1 C Program To Print Sentence using Ansi-style (c99)
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("C Programming");
+	return 0;
+}
