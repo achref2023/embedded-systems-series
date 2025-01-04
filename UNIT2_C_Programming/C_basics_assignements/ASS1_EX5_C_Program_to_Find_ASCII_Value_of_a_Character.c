25b09161223a79d927b946ecb5acffb64c5f8583
achref <achrfe.gmail.com>
2025-01-04 22:46:44 +0000

    ajout nouveaux exercices

--- UNIT2_C_Programming/C_basics_assignements/ASS1_EX5_C_Program_to_Find_ASCII_Value_of_a_Character.c
+++ UNIT2_C_Programming/C_basics_assignements/ASS1_EX5_C_Program_to_Find_ASCII_Value_of_a_Character.c
@@ -1,8 +1,8 @@
 /*
  ======================================================================================================================
  Name        : ASS5_C_Program_to_Find_ASCII_Value_of_a_Character.c
- Author      : Mina Karam
- Created on	 : Jul 30, 2021
+ Author      : Achref othmani
+ Created on	 : Jan 01, 2025
  Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX5 C Program to Find ASCII Value of a Character
  	 	 	   using Ansi-style (c99)
  ======================================================================================================================
@@ -12,12 +12,12 @@
 
 int main()
 {
-   char c;
+   char ch;
 
    printf("Enter a character: ");
-   scanf("%c",&c);
+   scanf("%c",&ch);
 
-   printf("ASCII value of %c = %d",c,c);
+   printf("ASCII value of %c = %d",ch,ch);
 
    return 0;
 }
